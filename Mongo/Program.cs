685c3cd563b66bf50d519f66f879edab76b48d69
VasilchukVV <VasilchukVV@gmail.com>
2020-02-12 01:38:25 +0300

    Initial commit

---
+++ Mongo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace Mongo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var client = new MongoClient(
+                "mongodb://localhost:27017/admin"
+            );
+            var database = client.GetDatabase("test");
+        }
+    }
+}
